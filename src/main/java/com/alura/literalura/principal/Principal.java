759d5949e287c90207c148885aee5fbcaa8f13db
Felipe Scheibler <felipe-scheibler@hotmail.com>
2024-12-30 00:11:17 -0400

    challenge Alura literalura

---
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -0,0 +1,162 @@
+package com.alura.literalura.principal;
+
+import com.alura.literalura.model.DadosLivro;
+import com.alura.literalura.model.Gutendex;
+import com.alura.literalura.model.Livro;
+import com.alura.literalura.service.AutorService;
+import com.alura.literalura.service.ConsumoApi;
+import com.alura.literalura.service.ConverteDados;
+import com.alura.literalura.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    @Autowired
+    private LivroService livroService;
+
+    @Autowired
+    private AutorService autorService;
+
+    private final String ENDERECO = "https://gutendex.com/books/?search=";
+
+    public void exibeMenu() {
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                    ----------------------------
+                    ESCOLHA O NÚMERO:
+                    1- Buscar livro pelo titulo
+                    2- Listar livros registrados
+                    3- Listar autores registrados
+                    4- Listar autores vivos em um determinado ano
+                    5- Listar livros em um determinado idioma
+                    0- Sair                                 
+                    ----------------------------
+                    """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    buscarLivroApi();
+                    break;
+                case 2:
+                    listarLivrosRegistrados();
+                    break;
+                case 3:
+                    listarAutoresRegistrados();
+                    break;
+                case 4:
+                    listarAutoresVivosNoAno();
+                    break;
+                case 5:
+                    listarLivrosPorIdioma();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+    }
+
+    public void buscarLivroApi() {
+        System.out.println("Insira o nome do livro que você deseja procurar:");
+        var nomeLivro = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeLivro.replace(" ", "%20"));
+
+        Gutendex dados = conversor.obterDados(json, Gutendex.class);
+
+        if (dados.results() != null && !dados.results().isEmpty()) {
+            for (DadosLivro livro : dados.results()) {
+                System.out.println(livro);
+
+                if (livroService.buscarPorTitulo(livro.titulo()).isEmpty()) {
+                    livroService.salvarLivro(livro);
+                } else {
+                    System.out.println("O livro '" + livro.titulo() + "' já está registrado no banco de dados.");
+                }
+            }
+        } else {
+            System.out.println("Nenhum resultado encontrado.");
+        }
+    }
+
+    public void listarLivrosRegistrados() {
+        List<String> livrosFormatados = livroService.listarLivros();
+        if (!livrosFormatados.isEmpty()) {
+            for (String livro : livrosFormatados) {
+                System.out.println(livro);
+            }
+        } else {
+            System.out.println("Nenhum livro registrado.");
+        }
+    }
+
+    public void listarAutoresRegistrados() {
+        List<String> autoresFormatados = autorService.listarAutores();
+        if (!autoresFormatados.isEmpty()) {
+            for (String autor : autoresFormatados) {
+                System.out.println(autor);
+            }
+        } else {
+            System.out.println("Nenhum autor registrado.");
+        }
+    }
+
+    public void listarAutoresVivosNoAno() {
+        System.out.println("Insira o ano que deseja pesquisar");
+        int ano = leitura.nextInt();
+        leitura.nextLine();
+
+        List<String> autoresVivos = autorService.listarAutoresVivosNoAno(ano);
+        if (!autoresVivos.isEmpty()) {
+            for (String autor : autoresVivos) {
+                System.out.println(autor);
+            }
+        } else {
+            System.out.println("Nenhum autor estava vivo no ano especificado.");
+        }
+    }
+
+
+    public void listarLivrosPorIdioma() {
+        System.out.println("Insira o idioma para realizar a busca:");
+        System.out.println("es- espanhol");
+        System.out.println("en- inglês");
+        System.out.println("fr- francês");
+        System.out.println("pt- português");
+
+        String idiomaSelecionado = leitura.nextLine();
+
+
+        List<Livro> livros = livroService.buscarLivrosPorIdioma(idiomaSelecionado);
+        if (!livros.isEmpty()) {
+            for (Livro livro : livros) {
+                System.out.println("-------------- Livro ----------------");
+                System.out.println("Título: " + livro.getTitulo());
+                System.out.println("Autores: " + livro.getAutores().stream()
+                        .map(autor -> autor.getNome())
+                        .reduce((a, b) -> a + ", " + b)
+                        .orElse("Nenhum autor encontrado"));
+                System.out.println("Idioma: " + livro.getIdioma());
+                System.out.println("Número de Downloads: " + livro.getNumeroDownloads());
+                System.out.println("--------------------------------------");
+            }
+        } else {
+            System.out.println("Não existem livros nesse idioma no banco de dados.");
+        }
+    }
+}
